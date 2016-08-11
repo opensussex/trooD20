d4e147ee97270deb7baa221999dacf06f8b9fe22
Graham Holden <opensussex@gmail.com>
2016-08-11 16:53:01 +0100

    create dice end point for rolling the dice

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-import "github.com/kataras/iris"
+import  "github.com/kataras/iris"
+import "math/rand"
+
 
 type response struct {
     Message string `json:"message"`
     Code string `json:"code"`
+    Value int `json:"value"`
 }
+
 
 func main() {
 
     iris.Get("/", welcome)
+    iris.Get("/dice", dice)
 
     iris.Listen(":8080")
 }
@@ -17,4 +22,14 @@ func main() {
 func welcome(ctx *iris.Context) {
     message := response{Message: "Welcome", Code: "200"}
     ctx.JSON(iris.StatusOK, message)
+}
+
+func dice(ctx *iris.Context) {
+    roll := roll()
+    message := response{Message: "Dice", Code: "200", Value: roll}
+    ctx.JSON(iris.StatusOK, message)
+}
+
+func roll() int {
+    return rand.Intn(20) + 1
 }
